ba9215339ecc31d6b2ee512397cfc00b2fc518d4
sumitb777 <bidwesumit9@gmail.com>
2026-01-14 17:32:59 +0530

    ruseme added

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,7 +1,7 @@
 export default function DownloadButton() {
   return (
     <a
-      href="/Sumit Bidwe Resume ph.pdf"
+      href="/DM-Resume.pdf"
       download
       className='px-6 py-3 rounded-full border border-white/10 
              bg-white/5 text-white backdrop-blur
